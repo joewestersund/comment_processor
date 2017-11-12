545770d9367bd582d33efc72052b1dbf751e693a
Joe Westersund <joe_westersund@hotmail.com>
2017-11-11 16:14:15 -0800

    add controllers for users and sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:edit, :edit_password, :update, :update_password, :show, :destroy, :index]
+  before_action :set_user, only: [:show, :edit, :edit_password, :update, :update_password, :destroy]
+
 
   # GET /users
   # GET /users.json
@@ -24,18 +26,16 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.new(user_params_new)
+    if @user.save
+      sign_in @user
+      flash[:notice] = "Welcome to the comment processor."
+      redirect_to welcome_path
+    else
+      render 'new'
     end
   end
+
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
@@ -46,6 +46,18 @@ class UsersController < ApplicationController
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update_password
+    respond_to do |format|
+      if params[:user][:password].present? and @user.update(user_params_change_password)
+        format.html { redirect_to profile_edit_password_path, notice: 'Your password was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit_password' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,28 @@
 Rails.application.routes.draw do
+
+  get 'sessions/new'
+
+  get 'static_pages/about'
+
+  get 'static_pages/welcome'
+
+  get '/about', to: 'static_pages#about' # creates named path 'about'
+  get '/welcome', to: 'static_pages#welcome' # creates named path 'welcome'
+  get '/signup', to: 'users#new' # creates named path 'signup'
+
   resources :users
+  get '/profile/edit_password', to: 'users#edit_password'
+  get '/profile/edit', to: 'users#edit'
+  patch '/profile/update_password', to: 'users#update_password'
+
   resources :categories
   resources :comments
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/signin', to: "sessions#new"
+  delete '/signout', to: "sessions#destroy"
+
+  root 'static_pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
