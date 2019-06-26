174bdf44fc34b75ea8660fdbf5580f90032867a4
Joe Westersund <joe_westersund@hotmail.com>
2019-06-25 19:23:25 -0700

    change app name and email address

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,5 @@
-APPLICATION_HOST_EMAIL_ADDRESS = 'comment-processor@joewestersund.net'
+APPLICATION_HOST_EMAIL_ADDRESS = 'response-to-comment@joewestersund.net'
 APPLICATION_HOST_NAME = "Joe Westersund"
-APPLICATION_EMAIL_ADDRESS = 'comment-processor@joewestersund.net'
-APPLICATION_NAME = "Comment Processor"
-APPLICATION_SHORT_NAME = "Comment Processor"
+APPLICATION_EMAIL_ADDRESS = 'response-to-comment@joewestersund.net'
+APPLICATION_NAME = "Response to Comment"
+APPLICATION_SHORT_NAME = "R2C"
