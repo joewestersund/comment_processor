654f6910b1f4342312ee4ef542307817a52503a2
Joe Westersund <joe_westersund@hotmail.com>
2019-06-26 17:32:08 -0700

    get SCSS and JS loading. Still some formatting issues.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,13 @@
 //= require popper
 //= require bootstrap-sprockets
 
+//= require bootstrap-multiselect
+//= require bootstrap-select
+//= require bootstrap/alert
+//= require bootstrap/dropdown
+
 //= require rails-ujs
 //= require trix
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
--- app/assets/javascripts/text_from_comments.js.coffee
+++ app/assets/javascripts/text_from_comments.js.coffee
@@ -1,5 +1,4 @@
 ready = ->
-  console.log "text_from_comments is loading"
   $('#show_text_from_comments').on 'click', () ->
     event.preventDefault()
     $('.text-from-comments').toggleClass('hidden')
