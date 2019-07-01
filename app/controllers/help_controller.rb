0b392e034f7e6a2b504485cb83de5c2c3d9556f7
Joe Westersund <joe_westersund@hotmail.com>
2019-07-01 15:25:11 -0700

    fix filtering for xlsx download for comments and suggested changes

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,8 +1,24 @@
 class HelpController < ApplicationController
 
   def excel_download_instructions
-    @download_url = params[:download_url]
-    @download_content_type = params[:download_content_type]
+    @object_type = params[:object_type]
+    @download_content_type = (@object_type == "Comment" ? "Comments" : "Suggested Changes")
+    @filter_querystring = excel_download_filter_params(@object_type)
+  end
+
+  private
+
+  def excel_download_filter_params(class_name)
+    if class_name == "Comment"
+      #from comments_controller.rb filter_params_all
+      params.permit(:first_name, :last_name, :email, :organization, :state, :comment_text, :summary, :comment_status_type_id, :notes, :manually_entered, :comment_data_source_id, :has_attachment, :suggested_change_id, )
+    elsif class_name == "SuggestedChange"
+      #from suggested_changes_controller.rb filter_params
+      params.permit(:suggested_change_name, :description, :response_text, :assigned_to_id, :suggested_change_status_type_id, :suggested_change_response_type_id, :action_needed, :rule_change_made, :notes, :text_from_comments)
+    else
+      raise "incorrect class_name '#{class_name}' passed to excel_download_filter_params"
+    end
+
   end
 
 end
--- app/helpers/excel_download_helper.rb
+++ app/helpers/excel_download_helper.rb
@@ -1,7 +1,18 @@
 module ExcelDownloadHelper
 
-  def excel_download_instructions_link(download_content_type,download_url)
-    "#{help_excel_download_instructions_path}?download_content_type=#{download_content_type}&download_url=#{download_url}"
+  def excel_download_instructions_link(klass,querystring)
+    "#{help_excel_download_instructions_path}?object_type=#{klass.name}&#{querystring.to_query}"
+  end
+
+  def excel_download_path(class_name,querystring)
+    if class_name == 'Comment'
+      download_link = comments_path(format: "xlsx") + "?#{querystring.to_query}"
+    elsif class_name == 'SuggestedChange'
+      download_link = suggested_changes_path(format: "xlsx") + "?#{querystring.to_query}"
+    else
+      raise "Error: unknown class '#{class_name}' passed to excel_download_path."
+    end
+    download_link
   end
 
 end
