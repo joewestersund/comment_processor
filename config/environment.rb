e504f1740cbb1648e75f388299035f927d09f0d3
Joe Westersund <joe_westersund@hotmail.com>
2020-12-30 19:08:08 -0800

    edit environment to use sendgrid API key

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,12 @@ require_relative 'application'
 Rails.application.initialize!
 
 # setting to send email using the Heroku SendMail add-on
+# edited 12/30/2020 to use API key instead of username + password
 ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    #:user_name => ENV['SENDGRID_USERNAME'],
+    #:password => ENV['SENDGRID_PASSWORD'],
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
     :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
