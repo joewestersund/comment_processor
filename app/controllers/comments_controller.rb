64a2d0d7bdff29ebaab4ca26e7f8c87a6a4f3ac9
Joe Westersund <joe_westersund@hotmail.com>
2021-10-20 20:08:34 -0700

    fix issue with authenticity token on push_import_add_attachment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,10 @@ class CommentsController < ApplicationController
   include ActionController::Live
   require 'csv'
 
-  skip_before_action :verify_authenticity_token, only: :do_push_import
+  skip_before_action :verify_authenticity_token, only: [:do_push_import, :do_push_import_add_attachment]
 
-  before_action :signed_in_user, except: [:do_push_import, :show_attachment]
-  before_action :user_with_permissions_to_a_rulemaking, except: [:do_push_import, :show_attachment]
+  before_action :signed_in_user, except: [:do_push_import, :do_push_import_add_attachment, :show_attachment]
+  before_action :user_with_permissions_to_a_rulemaking, except: [:do_push_import, :do_push_import_add_attachment, :show_attachment]
   before_action :admin_user, only: [:new, :create, :import, :destroy, :delete_attachment, :do_import, :cleanup, :do_cleanup]
   before_action :not_read_only_user, only: [:edit, :update]
   before_action :set_comment, only: [:show, :edit, :update, :destroy, :delete_attachment]
