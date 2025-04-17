4b7998b9d00cadfbca946ad6e7b6136a4925142c
Joe Westersund <joe_westersund@hotmail.com>
2025-04-16 21:18:53 -0700

    change style of select2 boxes

--- app/assets/javascripts/select2_initializer.js
+++ app/assets/javascripts/select2_initializer.js
@@ -1,9 +1,13 @@
 var ready;
 
 ready = function () {
-    $('.select2-multiselect').select2(
-        {theme: "bootstrap-5"}
-    );
+    var entity_name, firstRow, labels, pluralize;
+    entity_name = $('.select2-multiselect').data();
+    $('.select2-multiselect').select2({
+        theme: "bootstrap-5",
+        placeholder: 'Select a ' + entity_name.entityname + ', type here to search',
+        width: 'resolve'
+    })
 };
 
 $(document).on('turbo:load', ready);
